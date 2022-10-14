f1b1e398bbc08e251895c8487ef2b41f4814dc19
Michał <micbie981@gmail.com>
2022-10-14 17:28:29 +0200

    Styled images carousel

--- src/app/components/images-carousel/images-carousel.component.ts
+++ src/app/components/images-carousel/images-carousel.component.ts
@@ -1,20 +1,33 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'images-carousel',
   templateUrl: './images-carousel.component.html',
   styleUrls: ['./images-carousel.component.scss'],
 })
-export class ImagesCarouselComponent {
+export class ImagesCarouselComponent implements OnChanges {
   @Input() images: string[] = [];
-  private currentImage: number = 0;
+  currentImage: number = 0;
   get pathTransformExp(): string {
     return 'translateX(-' + 400 * this.currentImage + 'px)';
   }
 
-  constructor() {}
+  ngOnChanges(changes: SimpleChanges): void {
+    if (!changes['images']) return;
+    this.setCurrentImage(0);
+  }
 
   setCurrentImage(value: number) {
     this.currentImage = value;
+  }
+  handleLeftButtonClick() {
+    this.setCurrentImage(
+      this.currentImage === 0 ? this.images.length - 1 : --this.currentImage
+    );
+  }
+  handleRightButtonClick() {
+    this.setCurrentImage(
+      this.currentImage === this.images.length - 1 ? 0 : ++this.currentImage
+    );
   }
 }
