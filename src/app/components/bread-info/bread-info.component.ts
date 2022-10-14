e26b4a4488e2718ee67df5a9f9ffc0b2f036ebd9
Michał <micbie981@gmail.com>
2022-10-14 21:47:28 +0200

    Added link and title to bread info

--- src/app/components/bread-info/bread-info.component.ts
+++ src/app/components/bread-info/bread-info.component.ts
@@ -22,6 +22,24 @@ export class BreadInfoComponent implements OnChanges, OnDestroy {
   @Input() breadData: BreadData | undefined;
   imageUrls: string[] = [];
   subscription: Subscription | undefined;
+  get title(): string {
+    if (!this.breadData) return '';
+    const bread = this.firstLetterUpper(this.breadData.bread);
+    const subBread =
+      ' ' +
+      (this.breadData.subBread
+        ? this.firstLetterUpper(this.breadData.subBread)
+        : '');
+    return bread + subBread;
+  }
+  get link() {
+    if (!this.breadData) return '';
+    return (
+      'https://en.wikipedia.org/wiki/' +
+      this.breadData.bread +
+      (this.breadData.subBread ? `_${this.breadData.subBread}` : '')
+    );
+  }
 
   constructor(private breadImageService: BreadImagesService) {}
 
@@ -37,5 +55,9 @@ export class BreadInfoComponent implements OnChanges, OnDestroy {
 
   ngOnDestroy(): void {
     this.subscription?.unsubscribe();
+  }
+
+  firstLetterUpper(value: string) {
+    return value[0].toUpperCase() + value.substring(1);
   }
 }
