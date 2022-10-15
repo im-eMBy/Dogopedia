d6e08ee929e8278dc8c4ca5d246e4b5089261c76
Michał <micbie981@gmail.com>
2022-10-15 15:51:45 +0200

    Some tests for images carousel

--- src/app/components/images-carousel/images-carousel.component.spec.ts
+++ src/app/components/images-carousel/images-carousel.component.spec.ts
@@ -8,16 +8,31 @@ describe('ImagesCarouselComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ImagesCarouselComponent ]
-    })
-    .compileComponents();
+      declarations: [ImagesCarouselComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ImagesCarouselComponent);
     component = fixture.componentInstance;
+    component.images = ['test1', 'test2', 'test3'];
+    component.currentImage = 1;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should create image for every url passed', () => {
+    const imageElements = fixture.nativeElement.querySelectorAll(
+      '.carousel__image-container'
+    );
+    expect(imageElements.length).toEqual(component.images.length);
+  });
+  it('should update current image on right button click', () => {
+    component.handleRightButtonClick();
+    expect(component.currentImage).toEqual(2);
+  });
+  it('should update current image on left button click', () => {
+    component.handleLeftButtonClick();
+    expect(component.currentImage).toEqual(0);
   });
 });
